10c306c77acabbaebdcc1627e4ce05cc8981d0a8
OrlandoZuñiga <orlando.zuniga@sinco.com.co>
2021-04-07 12:35:27 -0500

    Cambio html

---
+++ view/assets/js/main.js
@@ -0,0 +1,21 @@
+
+const estudiante = document.querySelector(".materia");
+const titulo1 = document.querySelector(".titulo1");
+const titulo2 = document.querySelector(".titulo2");
+var tabla = document.getElementById('registros')
+
+
+
+titulo1.innerHTML = "MATERIA";
+//titulo2.innerHTML = "Nombre de la Materia";
+
+
+
+
+/*
+for (let i = 0; i < materia.length; i++) {
+    
+    tabla.insertRow(i-1).innerHTML =  <input type="text"></input> ;
+    
+}
+*/
